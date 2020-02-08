5559ab9965d938ff10e22ca650aa069b25f89901
Arshia Soleimanimoorchehkhorti <soleimaa@flip2.engr.oregonstate.edu>
2020-02-08 15:59:18 -0800

    added jsparticles

---
+++ main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from flask import *  
+app = Flask(__name__)  
+import popmodel
+from PIL import Image
+@app.route('/')  
+def upload():  
+    return render_template("index.html")  
+ 
+@app.route('/calculate/', methods = ['POST'])  
+def calculate():  
+    if request.method == 'POST':  
+        f = request.files['file'] 
+        f = Image.open(f)
+        print(type(f)) 
+        f = popmodel.doit(f)
+        #f.save(f.filename)  
+        resp = jsonify({"rsvp":f} )
+        return resp
+  
+if __name__ == '__main__':  
+
+    app.run(host = '0.0.0.0',port=4000,debug=True, threaded=True)
